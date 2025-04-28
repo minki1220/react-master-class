4b0b0e4f31b33b302cecaeae2b684dd28cd61857
minki1220 <130380152+minki1220@users.noreply.github.com>
2025-04-29 06:49:22 +0900

    styled-component
    Adapting and Extending
    
    -props를 사용하여 styled을 설정할 수 있다.
    -기존의 스타일을 그대로 와서 확장하는 것도 가능하다. const Circle = styled(Box) =>styled()를 이용하면 된다.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import "./App.css";
-
+import styled from "styled-components";
+const Father = styled.div`
+  display: flex;
+`;
+const Box = styled.div`
+  background-color: ${(props) =>
+    props.bgcolor}; // props 이름을 전달 받아서 사용
+  width: 100px;
+  height: 100px;
+`;
+const Circle = styled(Box)`
+  //Box의 모든 속성을 가지고 온 다음 추가하는 방법
+  border-radius: 50px;
+`;
+const Text = styled.span`
+  color: white;
+`;
 function App() {
-  return <div></div>;
+  return (
+    <Father>
+      <Box bgcolor="teal">
+        <Text>Hello</Text>
+      </Box>
+      <Circle bgcolor="tomato" />
+    </Father>
+  );
 }
 
 export default App;
